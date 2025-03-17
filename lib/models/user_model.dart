17d0457250293ddd1037b797ad20b9d7892f7ab1
aerengns <a.erengenis@gmail.com>
2025-03-17 13:26:08 +0100

    feat!: improved initial example CRUD.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,8 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:drive_or_drunk_app/config/constants.dart';
+
 class User {
   final String? id;
   final String name;
@@ -30,4 +35,39 @@ class User {
       'profilePicture': profilePicture,
     };
   }
+}
+
+Future<void> addUser(User user, FirebaseFirestore db) async {
+  if (user.id == null) {
+    db.collection(Collections.users).add(user.toMap());
+  } else {
+    await db.collection(Collections.users).doc(user.id).set(user.toMap());
+  }
+}
+
+Future<User?> getUser(String id, FirebaseFirestore db) async {
+  final doc = await db.collection(Collections.users).doc(id).get();
+  if (doc.exists) {
+    return User.fromMap(doc.data()!, doc.id);
+  }
+  return null;
+}
+
+Stream<List<User>> getUsers(FirebaseFirestore db) {
+  return db.collection(Collections.users).snapshots().map((snapshot) =>
+      snapshot.docs.map((doc) => User.fromMap(doc.data(), doc.id)).toList());
+}
+
+Future<void> updateUser(
+    String id, Map<String, dynamic> data, FirebaseFirestore db) async {
+  await db.collection(Collections.users).doc(id).update(data);
+}
+
+Future<void> deleteUser(String? id, FirebaseFirestore db) async {
+  if (id == null) {
+    log('Delete user called with null id');
+    // TODO: Handle error
+    return;
+  }
+  await db.collection(Collections.users).doc(id).delete();
 }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,44 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:drive_or_drunk_app/models/user_model.dart';
-
-class Collections {
-  static const String users = 'User';
-  static const String bookings = 'Booking';
-}
+import 'package:drive_or_drunk_app/models/user_model.dart' as user_model;
+import 'package:drive_or_drunk_app/models/user_model.dart' show User;
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
+// USER METHODS
   Future<void> addUser(User user) async {
-    if (user.id == null) {
-      _db.collection(Collections.users).add(user.toMap());
-    } else {
-      await _db.collection(Collections.users).doc(user.id).set(user.toMap());
-    }
+    return user_model.addUser(user, _db);
   }
 
   Future<User?> getUser(String id) async {
-    final doc = await _db.collection(Collections.users).doc(id).get();
-    if (doc.exists) {
-      return User.fromMap(doc.data()!, doc.id);
-    }
-    return null;
+    return user_model.getUser(id, _db);
   }
 
   Stream<List<User>> getUsers() {
-    return _db.collection(Collections.users).snapshots().map((snapshot) =>
-        snapshot.docs.map((doc) => User.fromMap(doc.data(), doc.id)).toList());
+    return user_model.getUsers(_db);
   }
 
   Future<void> updateUser(String id, Map<String, dynamic> data) async {
-    await _db.collection(Collections.users).doc(id).update(data);
+    return user_model.updateUser(id, data, _db);
   }
 
   Future<void> deleteUser(String? id) async {
-    if (id == null) {
-      // TODO: Handle error
-      return;
-    }
-    await _db.collection(Collections.users).doc(id).delete();
+    return user_model.deleteUser(id, _db);
   }
 }
