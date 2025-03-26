d877123f1728f57729c148366c47590c2ea08530
ChristianMariano4 <christian.mariano@mail.polimi.it>
2025-03-26 15:17:09 +0100

    feat: added custom clipper

--- lib/features/authentication/login_page.dart
+++ lib/features/authentication/login_page.dart
@@ -11,7 +11,8 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
@@ -28,13 +29,11 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
-      )..forward();
+    )..forward();
 
-    // Define an animation that scales the image from 1.0 to 0.4 
-    _animation = Tween<double>(
-      begin: 4.0,
-      end: 2.0
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+    // Define an animation that scales the image from 1.0 to 0.4
+    _animation = Tween<double>(begin: 4.0, end: 2.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
   }
 
   void _login() async {
@@ -60,8 +59,11 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Login'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // title: const Text('Login'),
         actions: [
           IconButton(
             icon: Icon(
@@ -74,99 +76,163 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
           ),
         ],
       ),
-      body: Column (
-        children: [
-          Container(
+      body: Column(children: [
+        ClipPath(
+          clipper: WavyClipper(),
+          child: Container(
             color: Colors.blue, //TODO: fix color
             child: AnimatedBuilder(
-            animation: _animation, 
-            builder: (context, child) {
-              return Transform.scale(
-                scale: _animation.value,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  alignment: Alignment.center,
-                  child: Image.asset('assets/logo_android12.png', width: 150,),
-                )
-              );
-            }
+                animation: _animation,
+                builder: (context, child) {
+                  return Transform.scale(
+                      scale: _animation.value,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        alignment: Alignment.center,
+                        child: Image.asset(
+                          'assets/logo_android12.png',
+                          width: 150,
+                        ),
+                      ));
+                }),
           ),
-          ),
-
-          
-          Form(
+        ),
+        Form(
             key: _formKey,
             child: Padding(
               padding: const EdgeInsets.all(24),
-              child: Column (
+              child: Column(
                 children: [
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffffc953), width: 1.5), //TODO: fix color
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xffffc953),
+                            width: 1.5), //TODO: fix color
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffffc953), width: 2)),
+                      fillColor: Colors.white,
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffffc953), width: 2)
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please enter an email' : null,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xffffc953),
+                            width: 1.5), //TODO: fix color
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffffc953), width: 2)),
+                      fillColor: Colors.white,
                     ),
-                    fillColor: Colors.white,
-                    ),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Please enter an email' : null,
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffffc953), width: 1.5), //TODO: fix color
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffffc953), width: 2)
-                    ),
-                    fillColor: Colors.white,
-                    ),
-                  obscureText: true,
-                  validator: (value) =>
-                      value!.isEmpty ? 'Please enter a password' : null,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _isLoading ? null : _login,
-                  child: const Text('Login'),
-                ),
-                const SizedBox(height: 16),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, AppRoutes.register);
-                  },
-                  child: const Text('Don’t have an account? Register'),
-                ),
+                    obscureText: true,
+                    validator: (value) =>
+                        value!.isEmpty ? 'Please enter a password' : null,
+                  ),
+                  const SizedBox(height: 16),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //TODO implement theese
+                      Text('Remember me'),
+                      Text('Forget Password?'),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _isLoading ? null : _login,
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        fixedSize: const Size(400, 60),
+                        backgroundColor: Colors.blue),
+                    child: const Text('Login',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('Don’t have an account?'),
+                      const SizedBox(width: 0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, AppRoutes.register);
+                        },
+                        child: const Text('SIGN UP',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
 
-                // ElevatedButton(
-                //   onPressed: _isLoading
-                //       ? null
-                //       : () {
-                //           context.read<AuthProvider>().signInWithGoogle();
-                //         },
-                //   child: Text('Sign in with Google'),
-                // ),
-                // ElevatedButton(
-                //   onPressed: _isLoading
-                //       ? null
-                //       : () {
-                //           context.read<AuthProvider>().signInWithFacebook();
-                //         },
-                //   child: Text('Sign in with Facebook'),
-                // ),
-              ],
-            ),
-          )),
-        ]
-      ),
+                  // ElevatedButton(
+                  //   onPressed: _isLoading
+                  //       ? null
+                  //       : () {
+                  //           context.read<AuthProvider>().signInWithGoogle();
+                  //         },
+                  //   child: Text('Sign in with Google'),
+                  // ),
+                  // ElevatedButton(
+                  //   onPressed: _isLoading
+                  //       ? null
+                  //       : () {
+                  //           context.read<AuthProvider>().signInWithFacebook();
+                  //         },
+                  //   child: Text('Sign in with Facebook'),
+                  // ),
+                ],
+              ),
+            )),
+      ]),
     );
   }
 }
 
+// Custom Clipper for Wavy Shape
+class WavyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0, size.height);
+
+    final firstCurve = Offset(0, size.height - 20);
+    final lastCurve = Offset(30, size.height - 20);
+    path.quadraticBezierTo(
+        firstCurve.dx, firstCurve.dy, lastCurve.dx, lastCurve.dy);
+
+    final secondFirstCurve = Offset(0, size.height - 20);
+    final secondLastCurve = Offset(size.width - 30, size.height - 20);
+    path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy,
+        secondLastCurve.dx, secondLastCurve.dy);
+
+    final thirdFirstCurve = Offset(size.width, size.height - 20);
+    final thirdLastCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(thirdFirstCurve.dx, thirdFirstCurve.dy,
+        thirdLastCurve.dx, thirdLastCurve.dy);
+
+    path.lineTo(size.width, 0);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
